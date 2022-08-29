2aeadcb7d0acd0d59d5263b152bbeb30772750b3
tuancm <tuancm@icd-vn.com>
2022-08-29 09:56:27 +0700

    Show data with FlatList

--- src/components/RepoComponent/RepoComponent.tsx
+++ src/components/RepoComponent/RepoComponent.tsx
@@ -5,10 +5,29 @@ import {
   TextInput,
   Button,
   StyleSheet,
+  FlatList,
+  ListRenderItem,
+  TouchableHighlight,
 } from "react-native";
 import { useAppDispatch, useAppSelector } from "app/hooks";
 import { RepoPending, stateRepo } from "./repoSlice";
 import { ProcessStatus } from "./repoTypes";
+
+const renderItem: ListRenderItem<string> = ({ item, index, separators }) => {
+  return (
+    <TouchableHighlight
+      key={index}
+      onPress={() => console.log(item)}
+      onShowUnderlay={separators.highlight}
+      onHideUnderlay={separators.unhighlight}
+      style={{ margin: 5, padding: 20, backgroundColor: "#cdcdcd" }}
+    >
+      <View>
+        <Text style={{ fontSize: 20, color: "blue" }}>{item}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
 
 const RepoComponent: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -17,16 +36,25 @@ const RepoComponent: React.FC = () => {
 
   const handleOnPress = () => {
     dispatch(RepoPending(text));
-  }
+  };
+
+  const onRefresh = () => {
+    dispatch(RepoPending(text));
+  };
 
   return (
     <View style={styles.con}>
       <Text>The title</Text>
       <TextInput style={styles.input} onChangeText={setText} value={text} />
       <Text>{loading === ProcessStatus.pending ? "Loading..." : "NO"}</Text>
-      <Button
-        title="Press me"
-        onPress={handleOnPress}
+      <Button title="Press me" onPress={handleOnPress} />
+      <FlatList
+        style={{backgroundColor: "#adadad"}}
+        data={data}
+        renderItem={renderItem}
+        onRefresh={onRefresh}
+        refreshing={loading === ProcessStatus.pending ? true : false}
+        ListEmptyComponent={<Text>Not Found SSS</Text>}
       />
     </View>
   );
