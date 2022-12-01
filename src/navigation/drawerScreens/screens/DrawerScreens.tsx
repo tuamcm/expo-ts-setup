192cf4aa7a4e29d45b172d48256c1285ccd2843c
tuancm <tuancm@icd-vn.com>
2022-12-01 13:46:49 +0700

    Fix title

--- src/navigation/drawerScreens/screens/DrawerScreens.tsx
+++ src/navigation/drawerScreens/screens/DrawerScreens.tsx
@@ -16,7 +16,7 @@ export const NotificationsScreen = ({ navigation }: any) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Button onPress={() => navigation.goBack()} title="Go back home" />
-      <Button onPress={() => navigation.openDrawer()} title="LLEEEADSF" />
+      <Button onPress={() => navigation.openDrawer()} title="Open Menu" />
     </View>
   );
 }
